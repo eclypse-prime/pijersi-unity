7c303e5d848fb88480cfcd8bcb43784d461811e8
LordTeflon <joaquim51370@gmail.com>
2023-02-06 17:03:35 +0100

    feat : auto-size

--- Assets/Scripts/UI/Menus/Tooltip.cs
+++ Assets/Scripts/UI/Menus/Tooltip.cs
@@ -4,15 +4,14 @@ using UnityEngine.InputSystem;
 
 public class Tooltip : MonoBehaviour
 {
+    [SerializeField] private RectTransform panel;
     [SerializeField] private TextMeshProUGUI text;
-    private RectTransform textTransform;
     private RectTransform canvas;
 
     public static Tooltip Instance { get; private set; }
 
     private void Awake()
     {
-        textTransform = text.rectTransform;
         canvas = GetComponent<RectTransform>();
 
         Instance = this;
@@ -22,23 +21,23 @@ public class Tooltip : MonoBehaviour
 
     public void Show(string content)
     {
-        textTransform.gameObject.SetActive(true);
+        panel.gameObject.SetActive(true);
         Vector3 mousePosition = Mouse.current.position.ReadValue();
-        textTransform.position = mousePosition;
+        panel.position = mousePosition;
 
         Vector2 pivot = Vector2.up;
         Vector2 scale = canvas.localScale;
-        if (mousePosition.x + textTransform.rect.width * scale.x > canvas.rect.width * scale.x)
+        if (mousePosition.x + panel.rect.width * scale.x > canvas.rect.width * scale.x)
             pivot.x = 1f;
-        if (mousePosition.y - textTransform.rect.height * scale.y < 0)
+        if (mousePosition.y - panel.rect.height * scale.y < 0)
             pivot.y = 0f;
-        textTransform.pivot = pivot;
-        Debug.Log((mousePosition, textTransform.rect.width, canvas.rect.width));
+        panel.pivot = pivot;
+
         text.text = content;
     }
 
     public void Hide()
     {
-        textTransform.gameObject.SetActive(false);
+        panel.gameObject.SetActive(false);
     }
 }
